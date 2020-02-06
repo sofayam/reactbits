6f4116be28a0efc8340094502e50ac6b1bc2f77f
Mark Andrew <mark.andrew@de.bosch.com>
2020-02-06 18:14:14 +0100

    busted shop stuff

--- client/src/Shop.tsx
+++ client/src/Shop.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 
 import './App.css';
 
 const Shop = () => {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+  const fetchItems = async () => {
+    const data = await fetch(
+      "https://fortnite-public-api.theapinetwork.com/prod9/upcoming/get"
+    );
+    const items = await data.json();
+    console.log(items);
+  }
   return (
     <div>
        <h1>Shop Page</h1>
